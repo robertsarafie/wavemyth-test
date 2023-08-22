3c36ecd0554cd97377c66f562cde48628f3f867f
robertsarafie <sarafierobert@gmail.com>
2023-08-22 19:00:54 +0300

    test-v0.2

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// // import Swiper bundle with all modules installed
-// import Swiper from 'swiper/bundle';
-
-// // import styles bundle
-// import 'swiper/css/bundle';
+'use strict';
 
 const navSlide = () => {
   const burger = document.querySelector('.burger');
@@ -17,13 +13,11 @@ const navSlide = () => {
 
     // Animate Links
     navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
-        }s`;
-      }
+      const getLinks = link.style.animation
+        ? (link.style.animation = '')
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.5
+          }s`);
     });
 
     // Burger animation
@@ -38,23 +32,15 @@ const swiper = new Swiper('.slide-content', {
   spaceBetween: 1000,
   centerSlide: 'true',
   grabCursor: true,
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-nextV2',
     prevEl: '.swiper-button-prevV2',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
